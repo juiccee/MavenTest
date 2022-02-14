d303d16590fe5e2b54392e9465df6228739540be
xiejun <xiejun@fjnx.com.cn>
2022-02-14 23:24:43 +0800

    modify

--- src/main/java/com/geekbang/oldstyle/UserLibraryInClassPathAppMain.java
+++ src/main/java/com/geekbang/oldstyle/UserLibraryInClassPathAppMain.java
@@ -4,7 +4,7 @@ import com.google.common.base.Splitter;
 
 public class UserLibraryInClassPathAppMain {
     public static void main(String[] args) {
-        String line = "  This is a     字符串    ";
+        String line = "  This is a     字符串 test   ";
         Iterable<String> words = Splitter.on(' ').trimResults().omitEmptyStrings().split(line);
         for (String word : words) {
             System.out.println(word);
